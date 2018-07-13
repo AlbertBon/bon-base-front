ceec494f2f63e1eaebc71c76a2f4e32920d5dde6
OfficeBon <502285815@qq.com>
2018-07-13 18:10:23 +0800

    新增表，修改表，创建表已完成

--- src/views/sys/index.vue
+++ src/views/sys/index.vue
@@ -12,57 +12,84 @@
       <el-button class="filter-item" @click="getList" style="margin-left: 10px;" type="primary" icon="el-icon-search">
         搜索
       </el-button>
-      <el-button class="filter-item" @click="getList" style="margin-left: 10px;" type="primary" icon="el-icon-circle-plus-outline">
+      <el-button class="filter-item" @click="addTable" style="margin-left: 10px;" type="primary" icon="el-icon-circle-plus-outline">
         新增表
       </el-button>
     </div>
 
     <div class="cur-table_info">
-      <div class="cur-table_title">表名：<el-input v-model="saveParams.tableName" :disabled="isNewTable" style="width:300px;" ></el-input></div>
-      <div class="cur-table_desc">表备注：<el-input v-model="saveParams.tableRemark" style="width:300px;" ></el-input></div>
+      <div class="cur-table_title">表名：
+        <el-input v-model="saveParams.tableName" :disabled="!isNewTable" style="width:300px;"></el-input>
+      </div>
+      <div class="cur-table_desc">表备注：
+        <el-input v-model="saveParams.tableRemark" style="width:300px;"></el-input>
+      </div>
     </div>
 
     <div class="filter-container" v-show="saveParams.tableName!=''">
       <el-button type="primary" size="mini" @click="addField()">新增一行</el-button>
       <el-button type="primary" size="mini" @click="saveTable()">保存</el-button>
+      <el-button type="primary" size="mini" @click="createTable()">创建数据库表</el-button>
     </div>
 
     <el-table :data="list" v-loading="listLoading" element-loading-text="给我一点时间" border fit
               highlight-current-row
               style="width: 100%">
+      <el-table-column width="150px" align="center" label="是否是主键id">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.isId" active-value="1" inactive-value="0"></el-switch>
+        </template>
+      </el-table-column>
       <el-table-column width="200px" align="center" label="字段名">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.fieldName" ></el-input>
+          <!--<el-form :model="scope.row" :rules="rules" ref="saveForm">-->
+          <!--<el-form-item prop="fieldName">-->
+          <!--<el-input v-model="scope.row.fieldName"></el-input>-->
+          <!--</el-form-item>-->
+          <!--</el-form>-->
+          <el-input v-model="scope.row.fieldName"></el-input>
         </template>
       </el-table-column>
       <el-table-column width="150px" align="center" label="字段类型">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.fieldType" ></el-input>
+          <el-select v-model="scope.row.fieldType" filterable placeholder="请选择">
+            <el-option
+              v-for="item in fieldTypeList"
+              :key="item.value"
+              :label="item.name"
+              :value="item.value">
+            </el-option>
+          </el-select>
         </template>
       </el-table-column>
       <el-table-column width="150px" align="center" label="字段长度">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.fieldLength" ></el-input>
+          <el-input v-model="scope.row.fieldLength"></el-input>
         </template>
       </el-table-column>
       <el-table-column width="100px" align="center" label="是否为空">
         <template slot-scope="scope">
-          <el-switch v-model="scope.row.isNull"></el-switch>
+          <el-switch v-model="scope.row.isNull" active-value="1" inactive-value="0"></el-switch>
         </template>
       </el-table-column>
       <el-table-column width="100px" align="center" label="是否唯一">
         <template slot-scope="scope">
-          <el-switch v-model="scope.row.isUnique"></el-switch>
+          <el-switch v-model="scope.row.isUnique" active-value="1" inactive-value="0"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column width="100px" align="center" label="是否为无符号">
+        <template slot-scope="scope">
+          <el-switch v-model="scope.row.isUnsigned" active-value="1" inactive-value="0"></el-switch>
         </template>
       </el-table-column>
       <el-table-column width="150px" align="center" label="默认值">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.defaultValue" ></el-input>
+          <el-input v-model="scope.row.defaultValue"></el-input>
         </template>
       </el-table-column>
       <el-table-column min-width="150px" label="字段备注">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.fieldRemark" ></el-input>
+          <el-input v-model="scope.row.fieldRemark"></el-input>
         </template>
       </el-table-column>
       <el-table-column align="center" label="操作" width="230" class-name="small-padding fixed-width">
@@ -70,6 +97,7 @@
           <el-button type="danger" size="mini" @click="deleteField(scope.$index, list)">删除</el-button>
         </template>
       </el-table-column>
+
     </el-table>
   </div>
 </template>
@@ -83,15 +111,34 @@
         listParams: {
           tableName: ''
         },
-        isNewTable:true,
+
+        isNewTable: false,
         list: [],
         listLoading: false,
 
-        saveParams:{
+        saveParams: {
           tableName: '',
           tableRemark: '',
-          fieldList:[]
-        }
+          fieldList: []
+        },
+        createParams:{},
+        rules: {
+          fieldName: [{required: true, message: '请输入字段名', trigger: 'blur'}],
+          fieldType: [{required: true, message: '请输入字段类型', trigger: 'blur'}],
+        },
+        fieldTypeList: [
+          {value: 'BIGINT', name: 'BIGINT'},
+          {value: 'INT', name: 'INT'},
+          {value: 'TINYINT', name: 'TINYINT'},
+          {value: 'DECIMAL', name: 'DECIMAL'},
+          {value: 'CHAR', name: 'CHAR'},
+          {value: 'VARCHAR', name: 'VARCHAR'},
+          {value: 'TEXT', name: 'TEXT'},
+          {value: 'DATETIME', name: 'DATETIME'},
+          {value: 'TIMESTAMP', name: 'TIMESTAMP'},
+          {value: 'MONEY', name: 'MONEY'},
+          {value: 'BIT', name: 'BIT'},
+        ]
       }
     },
     created() {
@@ -123,13 +170,21 @@
           }
         })
       },
-      addField(){
-        this.list.push({});
+      addField() {
+        this.list.push({isNull: '1'});
       },
-      deleteField(index,rows){
-        rows.splice(index,1);
+      deleteField(index, rows) {
+        rows.splice(index, 1);
       },
-      saveTable(){
+      //保存数据
+      saveTable() {
+        // this.$refs['saveForm'].validate((valid => {
+        //   if (valid) {
+        //     console.log("dui")
+        //   } else {
+        //     console.log("cuo")
+        //   }
+        // }))
         this.saveParams.fieldList = this.list;
         this.postRequest('/sys/saveTable', this.saveParams).then(res => {
           this.listLoading = false;
@@ -137,6 +192,25 @@
             this.$message.success('保存成功');
           }
         })
+      },
+      //根据系统表创建数据库表
+      createTable(){
+        this.createParams.tableName = this.saveParams.tableName;
+        this.createParams.tableRemark = this.saveParams.tableRemark;
+        this.postRequest('/sys/createTable', this.createParams).then(res => {
+          this.listLoading = false;
+          if (res.data.code == '00') {
+            this.$message.success('保存成功');
+          }
+        })
+      },
+      //新增表
+      addTable(){
+        this.saveParams.tableName = '';
+        this.saveParams.tableRemark = '';
+        this.list = [];
+        this.listParams.tableName = null;
+        this.isNewTable = true;
       }
 
     }
@@ -148,7 +222,8 @@
   .cur-table_info {
     padding-bottom: 20px;
   }
-  .cur-table_title{
+
+  .cur-table_title {
     padding: 10px 0;
   }
 </style>
